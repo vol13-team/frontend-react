64fcd4dd736c774d35b9187dc0b4e17fc175b161
taku10101 <taku1011ht@gmail.com>
2024-01-22 01:30:16 +0900

    rename done

--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import Article from "./Article";
+import Article from "./ArticlePage";
 import SidePanel from "../components/SidePanel";
 import { Link } from "react-router-dom";
 
