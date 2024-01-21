568488aa5901e82d04b11ef83e6dfdb7c2c6b2e4
taku10101 <taku1011ht@gmail.com>
2024-01-22 01:44:48 +0900

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./views/HomePage";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
