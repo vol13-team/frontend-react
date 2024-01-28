4c84d773af498e6da9f86c551c3ac12d39476eaf
taku10101 <taku1011ht@gmail.com>
2024-01-28 12:48:09 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,35 @@
-import { Route, Routes } from "react-router-dom";
-import "./App.css";
-import HomePage from "./views/HomePage";
-// import Article from "./views/ArticlePage";
-import ArticleIdPage from "./views/ArticleIdPage";
-import IssueIdPage from "./views/IssueIdPage";
-import AnswerPage from "./views/AnswerPage";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <>
-      <Routes>
-        {/*  */}
-        <Route path='/' element={<HomePage />} />
-        {/* 記事一覧 */}
-        <Route path='/article' element={<HomePage />} />
-        {/* 問題一覧 */}
-        <Route path='/article/:article_id' element={<ArticleIdPage />} />
-        {/* 問題表示 */}
-        <Route path='/article/:article_id/issue_id' element={<IssueIdPage />} />
-        {/* 答え合わせ */}
-        <Route
-          path='/article/:article_id/issue_id/answer'
-          element={<AnswerPage />}
-        />
-        <Route path='*' element={<h1>404</h1>} />
-      </Routes>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/thme.ts
+++
@@ -1,5 +0,0 @@
-export const theme = {
-  primaryColor: "#ff2b72",
-  secondaryColor: "#38cc2e",
-  // 他のカラーなども定義できます
-};
